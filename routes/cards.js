e1b82c808ebd7ed6ddf773c69325b02a268e2c6e
marialibkind <Maria07jyhjuytrh2006@yandex.ru>
2023-07-23 14:14:50 +0300

    new1

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,4 @@
 const cardsRouter = require("express").Router();
-const { celebrate } = require("celebrate");
 const auth = require("../middlewares/auth");
 const {
   getCards, createCard, deleteCard, addLikeCard, deleteLikeCard,
@@ -7,9 +6,9 @@ const {
 const { cardValidation, cardValidationId } = require("../utils/validate");
 
 cardsRouter.get("/cards", auth, getCards);
-cardsRouter.post("/cards", auth, celebrate(cardValidation), createCard);
-cardsRouter.delete("/cards/:cardId", auth, celebrate(cardValidationId), deleteCard);
-cardsRouter.put("/cards/:cardId/likes", auth, celebrate(cardValidationId), addLikeCard);
-cardsRouter.delete("/cards/:cardId/likes", auth, celebrate(cardValidationId), deleteLikeCard);
+cardsRouter.post("/cards", auth, cardValidation, createCard);
+cardsRouter.delete("/cards/:cardId", auth, cardValidationId, deleteCard);
+cardsRouter.put("/cards/:cardId/likes", auth, cardValidationId, addLikeCard);
+cardsRouter.delete("/cards/:cardId/likes", auth, cardValidationId, deleteLikeCard);
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,18 @@
 const usersRouter = require("express").Router();
-const { celebrate } = require("celebrate");
 const {
   getUsers, createUser, getUserId, setProfile, setAvatar, getInforCurrentUser, login,
 } = require("../controllers/user");
 const auth = require("../middlewares/auth");
 const { userValidation, userUpdateValidation } = require("../utils/validate");
 
-usersRouter.post("/signup", celebrate(userValidation), createUser);
-usersRouter.post("/signin", celebrate(userValidation), login);
+usersRouter.post("/signup", userValidation, createUser);
+usersRouter.post("/signin", userValidation, login);
 
 usersRouter.get("/users", auth, getUsers);
-usersRouter.get("/users/:userId", auth, celebrate(userValidation), getUserId);
+usersRouter.get("/users/:userId", auth, userValidation, getUserId);
 
 usersRouter.get("/users/me", auth, getInforCurrentUser);
-usersRouter.patch("/users/me", auth, celebrate(userUpdateValidation), setProfile);
-usersRouter.patch("/users/me/avatar", auth, celebrate(userUpdateValidation), setAvatar);
+usersRouter.patch("/users/me", auth, userUpdateValidation, setProfile);
+usersRouter.patch("/users/me/avatar", auth, userUpdateValidation, setAvatar);
 
 module.exports = usersRouter;
